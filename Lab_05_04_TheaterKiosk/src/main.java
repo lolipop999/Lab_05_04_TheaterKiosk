cf677301c92c368a9cb78dc39cc6b1dfbf977736
lolipop999 <106931888+lolipop999@users.noreply.github.com>
2022-06-07 14:54:36 -0400

    Add files via upload

---
+++ Lab_05_04_TheaterKiosk/src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+        int age = 16;
+        int WRIST_BAND_AGE = 21;
+
+        if (age >= WRIST_BAND_AGE)
+        {
+            System.out.println("You get a wristband.");
+        }
+        else
+        {
+            System.out.println("You are not old enough for a wristband");
+        }
+    }
+}
